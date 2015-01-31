ca36922ae4df506a49d1337cebc65ba8498314e4
David Björklund <david.bjorklund@gmail.com>
2015-01-31 12:34:11 +0100

    filters

--- index.js
+++ index.js
@@ -5,21 +5,28 @@ var sizeRegexp = /(\-?)(\d*)x(\-?)(\d*)\//;
 var halignRegexp = /(left|right|center)\//;
 var valignRegexp = /(top|bottom|middle)\//;
 var keywordsRegexp = /meta\/|smart\/fit-it\//g;
+var filtersRegexp = /filters:(.+)\//;
 
 module.exports.parseDecrypted = function (url) {
   var crop = url.match(cropRegexp);
   if (crop && crop[0]) url = url.replace(crop[0], '');
+
   var size = url.match(sizeRegexp);
   if (size && size[0]) url = url.replace(size[0], '');
+
   var halign = url.match(halignRegexp);
   if (halign && halign[0]) url = url.replace(halign[0], '');
+
   var valign = url.match(valignRegexp);
   if (valign && valign[0]) url = url.replace(valign[0], '');
+
   var meta = url.indexOf('meta/') !== -1;
   var smart = url.indexOf('smart/') !== -1;
   var fitIn = url.indexOf('fit-in/') !== -1;
-
   url = url.replace(keywordsRegexp, '');
+
+  var filters = url.match(filtersRegexp);
+  if (filters && filters[0]) url = url.replace(filters[0], '');
 
   return {
     image: url,
@@ -37,6 +44,7 @@ module.exports.parseDecrypted = function (url) {
     halign: (halign && halign[1]) || 'left',
     valign: (valign && valign[1]) || 'top',
     smart: smart,
-    fitIn: fitIn
+    fitIn: fitIn,
+    filters: (filters && filters[1]) || null
   };
 };
